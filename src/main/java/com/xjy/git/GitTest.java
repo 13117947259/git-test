5e46461dcb93e1cd7a769cc535bb3b0bfd9301ba
xjy <xjy@niubi.com>
2022-10-15 20:00:07 +0800

    first commit

---
+++ src/main/java/com/xjy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xjy.git;
+
+/**
+ * @author 熊建阳最帅
+ * @version 1.0
+ */
+@SuppressWarnings("all")
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,git!!!");
+    }
+}
